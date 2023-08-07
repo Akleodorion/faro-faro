571e2d7b823426c93ff48d5b85e40b5eff6b9588
Christian Bondzie <christian.zap@hotmail.fr>
2023-08-07 16:16:07 +0200

    Creation de la classe abstraire Failure qui va me permettre de définir le type d'érreur que je vais avoir

--- lib/features/user_authentification/domain/repositories/user_authentification_repository.dart
+++ lib/features/user_authentification/domain/repositories/user_authentification_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:faro_clean_tdd/core/errors/failures.dart';
 
 abstract class UserAuthentificationRepository {
-  Future<Either<void, void>>? logUserIn(String email, String password);
-  Future<Either<void, void>>? signUserIn(
+  Future<Either<Failure, void>>? logUserIn(String email, String password);
+  Future<Either<Failure, void>>? signUserIn(
       String email, String password, String username, String phoneNumber);
 }
