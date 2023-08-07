ab9de88edbabe0c30cda580399e7457a4cac2276
Christian Bondzie <christian.zap@hotmail.fr>
2023-08-07 22:29:14 +0200

    Ajout du usecase Sign User In et de son test

--- lib/features/user_authentification/domain/usecases/sign_user_in.dart
+++ lib/features/user_authentification/domain/usecases/sign_user_in.dart
@@ -1,1 +1,32 @@
-class SignUserIn {}
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:faro_clean_tdd/core/errors/failures.dart';
+import 'package:faro_clean_tdd/core/usecases/usecase.dart';
+import 'package:faro_clean_tdd/features/user_authentification/domain/repositories/user_authentification_repository.dart';
+
+class SignUserIn implements UseCase<bool, Params> {
+  SignUserIn({required this.repository});
+  final UserAuthentificationRepository repository;
+
+  @override
+  Future<Either<Failure, bool>>? call(Params params) {
+    return repository.signUserIn(
+        params.email, params.password, params.username, params.phoneNumber);
+  }
+}
+
+class Params extends Equatable {
+  final String email;
+  final String password;
+  final String username;
+  final String phoneNumber;
+
+  const Params(
+      {required this.email,
+      required this.password,
+      required this.username,
+      required this.phoneNumber});
+
+  @override
+  List<Object?> get props => [email, password, username, phoneNumber];
+}
---
+++ test/features/user_authentification/domain/usecases/sign_user_in_test.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:faro_clean_tdd/features/user_authentification/domain/entities/user.dart';
+import 'package:faro_clean_tdd/features/user_authentification/domain/repositories/user_authentification_repository.dart';
+import 'package:faro_clean_tdd/features/user_authentification/domain/usecases/sign_user_in.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'log_user_in_test.mocks.dart';
+
+@GenerateMocks([UserAuthentificationRepository])
+void main() {
+  late MockUserAuthentificationRepository mockUserAuthentificationRepository;
+  late SignUserIn signUserIn;
+  late String tEmail;
+  late String tPassword;
+  late String tUsername;
+  late String tPhoneNumber;
+
+  setUp(() {
+    mockUserAuthentificationRepository = MockUserAuthentificationRepository();
+    signUserIn = SignUserIn(repository: mockUserAuthentificationRepository);
+    tEmail = 'chris@gmail.com';
+    tPassword = "blabla123";
+    tUsername = "BakiHanma";
+    tPhoneNumber = "06 06 06 06 06";
+  });
+
+  test(
+    "should sign the user in",
+    () async {
+      //assert
+      when(mockUserAuthentificationRepository.signUserIn(any, any, any, any))
+          .thenAnswer((realInvocation) async => const Right(true));
+
+      //act
+      signUserIn(Params(
+          email: tEmail,
+          password: tPassword,
+          username: tUsername,
+          phoneNumber: tPhoneNumber));
+
+      //arrange
+      verify(mockUserAuthentificationRepository.signUserIn(
+          tEmail, tPassword, tUsername, tPhoneNumber));
+    },
+  );
+}
