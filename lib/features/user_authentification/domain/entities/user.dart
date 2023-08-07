477ec9d6b123c0c6c2688f33de507e2772317a88
Christian Bondzie <christian.zap@hotmail.fr>
2023-08-07 16:09:55 +0200

    Ajout de l'entité user

---
+++ lib/features/user_authentification/domain/entities/user.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User(
+      {required this.username, required this.email, required this.phoneNumber});
+
+  final String username;
+  final String email;
+  final String phoneNumber;
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
