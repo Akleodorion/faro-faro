19e85274ef814645c7c7c61b5559e8964952ccf0
Christian Bondzie <christian.zap@hotmail.fr>
2023-08-13 11:04:03 +0200

    Implémentation du user local data source et de ses fonctions en plus de ses tests unitaires

--- lib/features/user_authentification/data/datasources/user_local_data_source.dart
+++ lib/features/user_authentification/data/datasources/user_local_data_source.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class UserLocalDataSource {
@@ -5,7 +9,7 @@ abstract class UserLocalDataSource {
   Future<bool?> getLastPref();
 
   /// Gets the last email and password used for connexion.
-  Future<Map<String, String>?> getUserAuth();
+  Future<Map<String, dynamic>?> getUserAuth();
 
   /// Gets the last dateTime when the user was connected
   Future<DateTime?> getLastLoginDatetime();
@@ -13,13 +17,19 @@ abstract class UserLocalDataSource {
   ///Gets the last cached JWT token
   Future<String?> getLastCachedToken();
 
+  /// Stores all the connexion data in the cache
   Future<void>? storeConnexionData({
     required bool pref,
     required Map<String, String> userAuth,
-    required DateTime dateTime,
+    required String dateTime,
     required String jwtToken,
   });
 }
+
+const CACHED_JWT_TOKEN = 'CACHED_JWT_TOKEN';
+const CACHED_LOGIN_DATETIME = 'CACHED_LOGIN_DATETIME';
+const CACHED_CONNEXION_PREF = 'CACHED_CONNEXION_PREF';
+const CACHED_USER_AUTH_DATA = 'CACHED_USER_AUTH_DATA';
 
 class UserLocalDataSourceImpl implements UserLocalDataSource {
   const UserLocalDataSourceImpl({required this.sharedPreferences});
@@ -28,28 +38,51 @@ class UserLocalDataSourceImpl implements UserLocalDataSource {
 
   @override
   Future<String?> getLastCachedToken() async {
-    return null;
+    return sharedPreferences.getString(CACHED_JWT_TOKEN);
   }
 
   @override
   Future<DateTime?> getLastLoginDatetime() async {
-    return null;
+    final datetimeAsString = sharedPreferences.getString(CACHED_LOGIN_DATETIME);
+    if (datetimeAsString != null) {
+      return DateTime.parse(datetimeAsString);
+    } else {
+      return null;
+    }
   }
 
   @override
   Future<bool?> getLastPref() async {
-    return null;
+    final pref = sharedPreferences.getBool(CACHED_CONNEXION_PREF);
+    if (pref != null) {
+      return pref;
+    } else {
+      return false;
+    }
   }
 
   @override
-  Future<Map<String, String>?> getUserAuth() async {
-    return null;
+  Future<Map<String, dynamic>?> getUserAuth() async {
+    final userAuthData = sharedPreferences.getString(CACHED_USER_AUTH_DATA);
+    if (userAuthData != null) {
+      return json.decode(userAuthData);
+    } else {
+      return {
+        "email": "",
+        "password": "",
+      };
+    }
   }
 
   @override
   Future<void>? storeConnexionData(
       {required bool pref,
       required Map<String, String> userAuth,
-      required DateTime dateTime,
-      required String jwtToken}) async {}
+      required String dateTime,
+      required String jwtToken}) async {
+    sharedPreferences.setBool(CACHED_CONNEXION_PREF, pref);
+    sharedPreferences.setString(CACHED_USER_AUTH_DATA, json.encode(userAuth));
+    sharedPreferences.setString(CACHED_LOGIN_DATETIME, dateTime);
+    sharedPreferences.setString(CACHED_JWT_TOKEN, jwtToken);
+  }
 }
--- test/features/user_authentification/data/datasources/user_local_data_source_test.dart
+++ test/features/user_authentification/data/datasources/user_local_data_source_test.dart
@@ -1,8 +1,209 @@
+import 'dart:convert';
+
+import 'package:faro_clean_tdd/features/user_authentification/data/datasources/user_local_data_source.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+import 'user_local_data_source_test.mocks.dart';
 
 @GenerateMocks([SharedPreferences])
 void main() {
-  
+  late MockSharedPreferences mockSharedPreferences;
+  late UserLocalDataSourceImpl userLocalDataSourceImpl;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    userLocalDataSourceImpl =
+        UserLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
+  });
+
+  group(
+    "getLastCachedToken",
+    () {
+      const tToken =
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZWYyMGYwZC02OGY5LTQ5OTAtYjk4MC';
+      test(
+        "should get the last cached Token",
+        () async {
+          //assert
+          when(mockSharedPreferences.getString(CACHED_JWT_TOKEN))
+              .thenReturn(tToken);
+          //act
+          final result = await userLocalDataSourceImpl.getLastCachedToken();
+          //arrange
+          verify(mockSharedPreferences.getString(CACHED_JWT_TOKEN));
+          expect(result, tToken);
+        },
+      );
+    },
+  );
+
+  group(
+    "getLastLoginDatetime",
+    () {
+      final tDatetime = DateTime.now().toString();
+      test(
+        "should return null there is no Data",
+        () async {
+          //assert
+          when(mockSharedPreferences.getString(CACHED_LOGIN_DATETIME))
+              .thenReturn(null);
+          //act
+          final result = await userLocalDataSourceImpl.getLastLoginDatetime();
+          //arrange
+          expect(result, null);
+        },
+      );
+      test(
+        "should get the last cached Token",
+        () async {
+          //assert
+          when(mockSharedPreferences.getString(CACHED_LOGIN_DATETIME))
+              .thenReturn(tDatetime);
+          //act
+          final result = await userLocalDataSourceImpl.getLastLoginDatetime();
+          //arrange
+          verify(mockSharedPreferences.getString(CACHED_LOGIN_DATETIME));
+          expect(result, DateTime.parse(tDatetime));
+        },
+      );
+    },
+  );
+
+  group(
+    "getLastPref",
+    () {
+      test(
+        "should get the last cached connexion preference",
+        () async {
+          const tPref = true;
+
+          //assert
+          when(mockSharedPreferences.getBool(CACHED_CONNEXION_PREF))
+              .thenReturn(tPref);
+          //act
+          final result = await userLocalDataSourceImpl.getLastPref();
+          //arrange
+          verify(mockSharedPreferences.getBool(CACHED_CONNEXION_PREF))
+              .called(1);
+          expect(result, tPref);
+        },
+      );
+
+      test(
+        "should return false if there is no pref set",
+        () async {
+          const tPref = null;
+          //assert
+          when(mockSharedPreferences.getBool(CACHED_CONNEXION_PREF))
+              .thenReturn(tPref);
+          //act
+          final result = await userLocalDataSourceImpl.getLastPref();
+          //arrange
+          verify(mockSharedPreferences.getBool(CACHED_CONNEXION_PREF))
+              .called(1);
+          expect(result, false);
+        },
+      );
+    },
+  );
+
+  group(
+    "getUserAuth",
+    () {
+      test(
+        "should get the user email and password if present",
+        () async {
+          final Map<String, String> tUserAuth = {
+            "email": "test@gmail.com",
+            "password": "123456"
+          };
+          //assert
+          when(mockSharedPreferences.getString(CACHED_USER_AUTH_DATA))
+              .thenReturn(fixture('user_auth.json'));
+          //act
+          final result = await userLocalDataSourceImpl.getUserAuth();
+          //arrange
+
+          expect(result, tUserAuth);
+        },
+      );
+
+      test(
+        "should return an empty map if no data in cached",
+        () async {
+          //assert
+          when(mockSharedPreferences.getString(CACHED_USER_AUTH_DATA))
+              .thenReturn(null);
+          //act
+          final result = await userLocalDataSourceImpl.getUserAuth();
+          //arrange
+          verify(mockSharedPreferences.getString(CACHED_USER_AUTH_DATA))
+              .called(1);
+          final expectedResult = {"email": "", 'password': ""};
+          expect(result, expectedResult);
+        },
+      );
+    },
+  );
+
+  group(
+    "storeConnexionData",
+    () {
+      group(
+        "Store all the connexion Data ",
+        () {
+          const tPref = true;
+          final Map<String, String> tUserAuth = {
+            "email": "test@gmail.com",
+            "password": "123456"
+          };
+          final tDateTime = DateTime.now().toString();
+          const tToken =
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZWYyMGYwZC02OGY5LTQ5OTAtYjk4MC';
+
+          test(
+            "should store the pref setting",
+            () async {
+              //assert
+              when(mockSharedPreferences.setBool(any, any))
+                  .thenAnswer((_) async => true);
+              when(mockSharedPreferences.setString(CACHED_JWT_TOKEN, any))
+                  .thenAnswer((_) async => true);
+              when(mockSharedPreferences.setString(CACHED_LOGIN_DATETIME, any))
+                  .thenAnswer((_) async => true);
+              when(mockSharedPreferences.setString(CACHED_USER_AUTH_DATA, any))
+                  .thenAnswer((_) async => true);
+              //act
+              await userLocalDataSourceImpl.storeConnexionData(
+                  pref: tPref,
+                  userAuth: tUserAuth,
+                  dateTime: tDateTime,
+                  jwtToken: tToken);
+              //arrange
+              verify(mockSharedPreferences.setBool(
+                CACHED_CONNEXION_PREF,
+                tPref,
+              ));
+              verify(mockSharedPreferences.setString(
+                CACHED_JWT_TOKEN,
+                tToken,
+              ));
+              verify(mockSharedPreferences.setString(
+                CACHED_LOGIN_DATETIME,
+                tDateTime,
+              ));
+              verify(mockSharedPreferences.setString(
+                CACHED_USER_AUTH_DATA,
+                json.encode(tUserAuth),
+              ));
+            },
+          );
+        },
+      );
+    },
+  );
 }
