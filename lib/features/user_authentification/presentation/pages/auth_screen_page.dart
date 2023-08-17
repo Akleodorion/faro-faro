37294c300c2e6cf8258dc6030ff0ded20908f7ae
Christian Bondzie <christian.zap@hotmail.fr>
2023-08-17 11:08:04 +0200

    finition de la dépendance création de la auth page

---
+++ lib/features/user_authentification/presentation/pages/auth_screen_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(42, 43, 42, 1),
+              Color.fromRGBO(42, 43, 42, 0.2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
