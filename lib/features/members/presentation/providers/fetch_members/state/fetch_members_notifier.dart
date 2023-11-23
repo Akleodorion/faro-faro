de4f96d8e9ae201e1d755cef7dea85404166bc93
Christian Bondzie <christian.zap@hotmail.fr>
2023-11-23 02:10:25 +0100

    Ajout du provider fetch members

---
+++ lib/features/members/presentation/providers/fetch_members/state/fetch_members_notifier.dart
@@ -0,0 +1,24 @@
+import 'package:faro_clean_tdd/core/errors/failures.dart';
+import 'package:faro_clean_tdd/features/members/domain/usecases/fetch_members_usecase.dart';
+import 'package:faro_clean_tdd/features/members/presentation/providers/fetch_members/state/fetch_members_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FetchMemberNotifier extends StateNotifier<FetchMembersState> {
+  FetchMemberNotifier({required this.usecase}) : super(Initial());
+  final FetchMembersUsecase usecase;
+
+  FetchMembersState get initialState => Initial();
+
+  Future<FetchMembersState?> fetchMember({required int userId}) async {
+    state = Loading();
+    final result = await usecase.execute(userId: userId);
+    result!.fold((failure) {
+      if (failure is ServerFailure) {
+        state = Error(message: failure.errorMessage);
+      }
+    }, (members) {
+      state = Loaded(members: members);
+    });
+    return state;
+  }
+}
---
+++ lib/features/members/presentation/providers/fetch_members/state/fetch_members_state.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:faro_clean_tdd/features/members/domain/entities/member.dart';
+
+class FetchMembersState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class Initial extends FetchMembersState {}
+
+class Loading extends FetchMembersState {}
+
+class Loaded extends FetchMembersState {
+  Loaded({required this.members});
+  final List<Member> members;
+}
+
+class Error extends FetchMembersState {
+  Error({required this.message});
+  final String message;
+}
