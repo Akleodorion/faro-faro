cc75b8baa68dafe04f16f4e1a74e6cbb1e05c6b2
Christian Bondzie <christian.zap@hotmail.fr>
2023-11-27 02:31:00 +0100

    Ajout du widget en cas de -de 10 event.

--- lib/pages/home_page/sections/event_section/event_section.dart
+++ lib/pages/home_page/sections/event_section/event_section.dart
@@ -1,5 +1,7 @@
+import 'package:faro_clean_tdd/features/events/domain/entities/event.dart';
+import 'package:faro_clean_tdd/pages/event_show_page/event_show_page.dart';
+
 import '../../../../features/events/presentation/providers/fetch_event/fetch_event_provider.dart';
-import '../../../../main.dart';
 import 'widgets/list_view/list_view_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,10 +18,102 @@ class EventSection extends ConsumerWidget {
     final allEvents = ref.read(allEventProvider);
 
     if (allEvents.length < 10) {
-      content = Center(
-        child: Text(
-          "Il n'y a pas d'évènement en cours",
-          style: theme.textTheme.titleLarge,
+      content = Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Column(
+          children: [
+            const Text("Hello World"),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  crossAxisCount: 2,
+                ),
+                children: [
+                  for (final event in allEvents)
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (builder) {
+                          return EventShowPage(event: event);
+                        }));
+                      },
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: kElevationToShadow[3]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 190,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(5),
+                                    topRight: Radius.circular(5),
+                                  ),
+                                  image: DecorationImage(
+                                    image: NetworkImage(event.imageUrl),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                event.name,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Text(
+                                "${event.formatedDate} - ${event.address.addressName}",
+                                style: Theme.of(context).textTheme.titleSmall,
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "A partir de:",
+                                    style:
+                                        Theme.of(context).textTheme.titleSmall,
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    event.modelEco == ModelEco.gratuit
+                                        ? "Gratuit"
+                                        : "${event.standardTicketPrice} XOF",
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
     } else {
