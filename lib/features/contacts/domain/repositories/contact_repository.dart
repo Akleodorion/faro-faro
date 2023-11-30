75147cc34bda2c45e2e44b417a80fda9413e704a
Christian Bondzie <christian.zap@hotmail.fr>
2023-11-30 06:26:29 +0100

    Création du contrat contact repository

---
+++ lib/features/contacts/domain/repositories/contact_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:faro_clean_tdd/features/contacts/domain/entities/contact.dart';
+
+import '../../../../core/errors/failures.dart';
+
+abstract class ContactRepository {
+  //Récupère la liste des contacts de l'utilisateur connecté.
+  Future<Either<Failure, List<Contact>>>? fectchConctacts();
+}
