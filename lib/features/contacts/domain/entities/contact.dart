c547084847f33b415c6c11c667487e1e1c399566
Christian Bondzie <christian.zap@hotmail.fr>
2023-11-30 06:13:00 +0100

    Création de l'entité contact

---
+++ lib/features/contacts/domain/entities/contact.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+class Contact extends Equatable {
+  const Contact({
+    required this.userId,
+    required this.phoneNumber,
+    required this.username,
+  });
+
+  final int userId;
+  final String phoneNumber;
+  final String username;
+
+  @override
+  List<Object?> get props => [
+        userId,
+        phoneNumber,
+        username,
+      ];
+}
